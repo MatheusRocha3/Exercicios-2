94a329a62eea251c68dff4c4c25b32bea1720b4d
MatheusRocha3 <matheuusrocha012@gmail.com>
2022-06-22 20:13:34 -0300

    Mudança de Hello World para Olá Mundo

--- src/Exercicio1/main.java
+++ src/Exercicio1/main.java
@@ -1,12 +1,14 @@
 package Exercicio1;
 
 public class main {
-    public static void main(String[] args) {
-        System system;
-        System.out.println("Hello World!");
+        public static void main(String[] args) {
+            System system;
+            System.out.println("Olá Mundo!");
+        }
+
     }
 
-}
+
 
 
 
