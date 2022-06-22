496fc8388d8776ec4efd34ac9d0c1054397456a0
MatheusRocha3 <matheuusrocha012@gmail.com>
2022-06-22 20:02:39 -0300

    Adicionando formato ao exercicio7

--- src/Exercicio7/Salário.java
+++ src/Exercicio7/Salário.java
@@ -1,12 +1,12 @@
 package Exercicio7;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Salário {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double bruto = 0;
+        double bruto = 0.0;
         double ImpostoDerenda = 0;
         double sindicato = 0;
         double inss = 0;
@@ -17,26 +17,19 @@ public class Salário {
         double mês = input.nextDouble();
         input.close();
 
-        salario = (horas * mês);
+        var formatter = new DecimalFormat("###.##");
 
+        salario = (horas * mês);
 
         inss = (salario * 8) / 100;
         sindicato = (salario * 5) / 100;
         ImpostoDerenda = (salario * 11) / 100;
         bruto = (salario - inss - sindicato - ImpostoDerenda);
 
-        System.out.println("Seu salário bruto é: " + bruto + "R$");
+        System.out.println("Seu salário bruto é: " + formatter.format(bruto) + "R$");
         System.out.println("Você pagou para o inss: " + inss + "R$");
         System.out.println("Você pagou para o sindicato: " + sindicato + "R$");
         System.out.println("Seu salário liquido é: " + salario + "R$");
-
-
-
-
-
-
-
-
     }
 }
 
