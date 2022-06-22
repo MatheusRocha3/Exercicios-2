b774244eb19b21b5f07bc4646828c65cb4cb8066
MatheusRocha3 <matheuusrocha012@gmail.com>
2022-06-22 18:19:59 -0300

    Mudança de Int para Double. Não fazia sentido manter em int

--- src/Exercicio7/Salário.java
+++ src/Exercicio7/Salário.java
@@ -6,15 +6,15 @@ public class Salário {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int bruto = 0;
-        int ImpostoDerenda = 0;
-        int sindicato = 0;
-        int inss = 0;
-        int salario = 0;
+        double bruto = 0;
+        double ImpostoDerenda = 0;
+        double sindicato = 0;
+        double inss = 0;
+        double salario = 0;
         System.out.print("Digite o valor da sua hora trabalhada: ");
-        int horas = input.nextInt();
+        double horas = input.nextDouble();
         System.out.println("Digite quantas horas você trabalha por mês: ");
-        int mês = input.nextInt();
+        double mês = input.nextDouble();
         input.close();
 
         salario = (horas * mês);
@@ -23,7 +23,7 @@ public class Salário {
         inss = (salario * 8) / 100;
         sindicato = (salario * 5) / 100;
         ImpostoDerenda = (salario * 11) / 100;
-        bruto = (salario + inss + sindicato + ImpostoDerenda);
+        bruto = (salario - inss - sindicato - ImpostoDerenda);
 
         System.out.println("Seu salário bruto é: " + bruto + "R$");
         System.out.println("Você pagou para o inss: " + inss + "R$");
